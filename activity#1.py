b902ea63517b81a7592c6c7b6a9dea9bb6cefa21
mahad446 <mahadmubashir43@gmail.com>
2025-01-18 14:18:35 -0800

    activity#1

---
+++ activity#1.py
@@ -0,0 +1,16 @@
+v = 4
+w = 5
+x = 8
+y = 2
+z = 0
+z = (v+w) * x/ y;
+print ("value of (v+w) * is ", z)
+
+
+name = "mahad"
+age  = 13
+
+if name == "mahad" or name == "john" and age >= 2 :
+    print("hello welcome mahad")
+else:
+    print("good bye mahad!!")
